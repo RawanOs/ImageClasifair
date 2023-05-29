019fc62ff33dbefaaed39540aa01d177ce30ee30
RawanOs <89946063+RawanOs@users.noreply.github.com>
2023-05-29 18:19:05 +0300

    Initial Commit

---
+++ ImageClasifair/ImageClasifairApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageClasifairApp.swift
+//  ImageClasifair
+//
+//  Created by Rawan on 09/11/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct ImageClasifairApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
